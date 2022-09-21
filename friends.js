c43cd66cf5b337d7c165f3e99090ab00bf803029
Rick Vermeil <rickv85@gmail.com>
2022-09-21 15:52:48 -0600

    Add places var

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Alice", "Betty", "Charlie", "David", "Ethel"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var places = ['Atlanta', 'Boston', 'Cleveland', 'Denver', 'Ethiopia']
+console.log(places.slice(1, 4))
